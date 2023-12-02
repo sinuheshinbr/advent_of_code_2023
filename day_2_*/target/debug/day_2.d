c4d21418664815f578083150214b9f3c6f9b9938
sinuheshinbr <sinuheshin@gmail.com>
2023-12-02 08:41:12 -0300

    feat: finished day 2 first part

---
+++ day_2_*/target/debug/day_2.d
@@ -0,0 +1,1 @@
+/home/sinuheshin/github/advent_of_code_2023/day_2/target/debug/day_2: /home/sinuheshin/github/advent_of_code_2023/day_2/src/main.rs
---
+++ day_2_*/target/debug/deps/day_2-b3c8123f46067255.d
@@ -0,0 +1,5 @@
+/home/sinuheshin/github/advent_of_code_2023/day_2/target/debug/deps/day_2-b3c8123f46067255.rmeta: src/main.rs
+
+/home/sinuheshin/github/advent_of_code_2023/day_2/target/debug/deps/day_2-b3c8123f46067255.d: src/main.rs
+
+src/main.rs:
