ff3b077dcb4fdc24aa1b579a7573c7aab2e54ce6
sinuheshinbr <sinuheshin@gmail.com>
2023-12-04 07:10:37 -0300

    feat: finished day 3 first part

---
+++ day_3_*/target/debug/deps/day_3-7097497a3cc8e1bc.d
@@ -0,0 +1,5 @@
+/home/sinuheshin/github/advent_of_code_2023/day_3/target/debug/deps/day_3-7097497a3cc8e1bc.rmeta: src/main.rs
+
+/home/sinuheshin/github/advent_of_code_2023/day_3/target/debug/deps/day_3-7097497a3cc8e1bc.d: src/main.rs
+
+src/main.rs:
